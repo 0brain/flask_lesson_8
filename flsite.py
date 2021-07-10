11c78826e1451d49123c9f36910434d53b10a9ab
nazar <nazar.khort@gmail.com>
2021-07-10 23:33:44 +0300

    Ввів функцію для встановлення зєднання з базою даних та функцію яка буде створювати базу даних.

--- flsite.py
+++ flsite.py
@@ -10,3 +10,17 @@ SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'  # ввів секретний ключ
 app = Flask(__name__)
 app.config.from_object(__name__)  # загрузив конфігурацію з поточної програми, тобто на основі змінних DATABASE, DEBUG, SECRET_KEY буде сформована початкова конфігурація
 app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))  # вказуємо шлях до бази даних, вона буде знаходитися в робочому каталозі нашої програми
+
+
+def connect_db():  # Функція для встановлення зєднання з базою даних
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():  # Функція яка буде створювати базу даних
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
