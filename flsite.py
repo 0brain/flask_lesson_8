3a0c09a2e91f1d10044936a21b12d04b4265b9e5
nazar <nazar.khort@gmail.com>
2021-07-10 23:23:50 +0300

    Визначив конфігураційні значення. Вказав шлях до бази даних.

--- flsite.py
+++ flsite.py
@@ -1,3 +1,12 @@
 import sqlite3
 import os
 from flask import Flask, render_template, request
+
+# конфигурация
+DATABASE = '/tmp/flsite.db'  # шлях до бази даних
+DEBUG = True  # щоб було видно помилки
+SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'  # ввів секретний ключ
+
+app = Flask(__name__)
+app.config.from_object(__name__)  # загрузив конфігурацію з поточної програми, тобто на основі змінних DATABASE, DEBUG, SECRET_KEY буде сформована початкова конфігурація
+app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))  # вказуємо шлях до бази даних, вона буде знаходитися в робочому каталозі нашої програми
