91da66374343bc43923de0e65af3de9b3b737327
nazar <nazar.khort@gmail.com>
2021-07-11 10:47:58 +0300

    Додав пропущений пробіл і видалив комент

--- flsite.py
+++ flsite.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'  # ввів секретний ключ
 
 app = Flask(__name__)
 app.config.from_object(__name__)  # загрузив конфігурацію з поточної програми, тобто на основі змінних DATABASE, DEBUG, SECRET_KEY буде сформована початкова конфігурація
-app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))  # вказуємо шлях до бази даних, вона буде знаходитися в робочому каталозі нашої програми
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))  # вказуємо шлях до бази даних, вона буде знаходитися в робочому каталозі нашої програми
 
 
 def connect_db():  # Функція для встановлення зєднання з базою даних
--- sq_db.sql
+++ sq_db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS mainmenu (  # Створюємо таблицю mainmenu, якщо вона ще не існує
+CREATE TABLE IF NOT EXISTS mainmenu (
 id integer PRIMARY KEY AUTOINCREMENT,
 title text NOT NULL,
 url text NOT NULL
