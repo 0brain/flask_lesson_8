84e226c8c9f4978d79695ec6dbf546e0cf26cda9
nazar <nazar.khort@gmail.com>
2021-07-12 18:01:58 +0300

    Debug. Додав url.

--- FDataBase.py
+++ FDataBase.py
@@ -2,6 +2,7 @@ import sqlite3
 import time
 import math
 
+
 class FDataBase:
     def __init__(self, db):  # посилання на звязок з базою даних db
         self.__db = db  # ми зберігаємо посилання в екземплярі цього класу
@@ -17,10 +18,11 @@ class FDataBase:
             print("Помилка читання з бази даних")
         return []
 
-    def addPost(self, title, text):  # метод додавання статті приймає 2 параметри: title, text
+
+    def addPost(self, title, text, url):  # метод додавання статті приймає 2 параметри: title, text
         try:  # в блоці try намагаємося додати запис в базу даних
             tm = math.floor(time.time())  # отримуємо поточний час додавання статті, округлений до секунд
-            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)", (title, text, tm))  # добавляємо записи в таблицю posts і беремо дані з кортежу (title, text, tm)
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)", (title, text, url, tm))  # добавляємо записи в таблицю posts і беремо дані з кортежу (title, text, url, tm)
             self.__db.commit()  # зберігає в базу даних цей запис "INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)"
         except sqlite3.Error as e:  # якщо при додаванні виникла помилка, то
             print("Помилка додавання статті в БД "+str(e))  # виводимо повідомлення про помилку і повертаємо False
