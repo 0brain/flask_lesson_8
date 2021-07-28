7d06a25bafc8ad0be28d368f6748db7249b5a4c1
nazar <nazar.khort@gmail.com>
2021-07-28 22:27:22 +0300

    Додав кілька hashів, щоб розібратися в wtforms.

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm  # FlaskForm - базовий клас, який ми будемо розширювати за допомогою дочірнього класу LoginForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
 
-class LoginForm(FlaskForm):
-    email = StringField("Email: ", validators=[Email("Некоректний email")])
+class LoginForm(FlaskForm):  # LoginForm - клас для форми авторизації
+    email = StringField("Email: ", validators=[Email("Некоректний email")])  # StringField - клас для роботи з полем введення # змінна email буде зсилатися на екземпляр класу StringField, назва поля "Email: ", параметр validators, який буде зсилатися на список валідаторів необхідних для перевірки коректності введених даних в полі емейл
     psw = PasswordField("Пароль: ", validators=[DataRequired(),
-                                                Length(min=4, max=100, message="Пароль повинен бути від 4 до 100 символів")])
-    remember = BooleanField("Запам’ятати", default = False)
-    submit = SubmitField("Увійти")
+                                                Length(min=4, max=100, message="Пароль повинен бути від 4 до 100 символів")])  # PasswordField клас для роботи з полем введення паролю # DataRequired() - вимагає, щоб в даному полі був хоча би 1 символ
+    remember = BooleanField("Запам’ятати", default = False)  # BooleanField - клас для чекбокс полів # за замовчуванням False, тобто не встановлена галочка запамятати
+    submit = SubmitField("Увійти")  # SubmitField - клас для кнопки submit
