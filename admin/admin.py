184fc56eee91d37478f702b5ae41f4d6bf93e386
nazar <nazar.khort@gmail.com>
2021-07-31 17:44:14 +0300

    Ввів admin - екземпляр класу Blueprint, та перечислив його параметри: 'admin', __name__, template_folder, static_folder

--- admin/admin.py
+++ admin/admin.py
@@ -1,1 +1,9 @@
 from flask import Blueprint
+
+# ввів admin - екземпляр класу Blueprint, та перечислив його параметри: 'admin', __name__, template_folder, static_folder
+admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+#'admin' - ім'я Blueprint, яке буде суфіксом до всіх імен методів, даного модуля
+#__name__ - ім'я виконуваного модуля, щодо якого буде шукатися папка admin і відповідні підкаталоги
+#template_folder - підкаталог для шаблонів
+#static_folder - підкаталог для статичних файлів
+
